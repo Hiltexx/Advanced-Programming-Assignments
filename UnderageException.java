64c893cf988d910d54afbfeb4885e88a75d763cb
Hilton Boro <hiltonboro000@gmail.com>
2026-06-10 11:46:06 +0530

    Update UnderageException.java

--- UnderageException.java
+++ UnderageException.java
@@ -1,1 +1,12 @@
-
+package com.onboarding;
+public class UnderageException extends RuntimeException {
+    private final int providedAge;
+    private final int minimumAge;
+    public UnderageException(int providedAge, int minimumAge) {
+        super("Applicant age " + providedAge + " does not meet the minimum age requirement of " + minimumAge + ". Registration denied.");
+        this.providedAge = providedAge;
+        this.minimumAge = minimumAge;
+    }
+    public int getProvidedAge() { return providedAge; }
+    public int getMinimumAge() { return minimumAge; }
+}
