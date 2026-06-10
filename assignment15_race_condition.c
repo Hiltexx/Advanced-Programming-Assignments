14f609b4063b964aadd88a45bed6a6fbc0272d07
Hilton Boro <hiltonboro000@gmail.com>
2026-06-10 11:40:45 +0530

    Update assignment15_race_condition.c

--- assignment15_race_condition.c
+++ assignment15_race_condition.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+#include <pthread.h>
 
+#define THREADS 4
+#define INCREMENTS 1000000
+
+long long counter = 0;
+
+void* increment(void* arg)
+{
+    for (int i = 0; i < INCREMENTS; i++)
+    {
+        counter++;   // Critical section (unsafe)
+    }
+
+    return NULL;
+}
+
+int main()
+{
+    pthread_t tid[THREADS];
+
+    // Create threads
+    for (int i = 0; i < THREADS; i++)
+    {
+        pthread_create(&tid[i], NULL, increment, NULL);
+    }
+
+    // Wait for threads to finish
+    for (int i = 0; i < THREADS; i++)
+    {
+        pthread_join(tid[i], NULL);
+    }
+
+    printf("Final Counter Value = %lld\n", counter);
+
+    return 0;
+}
